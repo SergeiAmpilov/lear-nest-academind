aa772ab295165b4d305c10275099c345aa1927fa
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-07-03 18:52:46 +0300

    feat: senf json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('json')
+  getJson(): { [key: string]: number } {
+    return this.appService.getJson()
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getJson(): { [key: string]: number } {
+
+    return {
+      max: 41,
+      oksana: 23
+    }
+
+  }
 }
