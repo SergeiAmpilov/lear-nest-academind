60d5dc471ebc363faea5943f5fa602807ab619e9
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-07-04 11:00:00 +0300

    feat: post controller ref

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,24 @@
-import { Post, Controller } from "@nestjs/common";
+import { Post, Controller, Body } from "@nestjs/common";
+import { ProductsService } from "./products.service";
+import { Product } from "./product.model";
 
 
 @Controller('products')
 export class ProductsController {
 
+  constructor(
+    private readonly productService: ProductsService
+  ) {}
+
   @Post()
-  addProduct(): any {
-    
+  addProduct(
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ): {id: string} {
+    const generatedId: string = this.productService.insertProduct(title, description, price);
+    return {
+      id: generatedId
+    }
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,5 +4,14 @@ import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[];
+  products: Product[] = [];
+
+  insertProduct(title: string, description: string, price: number): string {
+    const productId: string = new Date().toString();
+    const newProd = new Product(productId, title, description, price);    
+    this.products.push(newProd);
+
+    return productId;
+
+  }
 }
